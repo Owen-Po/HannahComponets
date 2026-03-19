859da731fb745f52923a17a51e24c92a9cdeafa3
Owen-Po <owen.pinedo@hannahlab.com>
2026-03-19 16:50:51 -0500

    fix : se arreglo pequeños detalles en la importacion.

--- hannah-ui/vite.config.ts
+++ hannah-ui/vite.config.ts
@@ -8,27 +8,39 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    dts()
+    dts({
+      insertTypesEntry: true,
+      rollupTypes: true,
+    }),
   ],
 
   build: {
     lib: {
       entry: resolve(import.meta.dirname, 'src/main.ts'),
-      name: 'Hannah-UI',
-
+      name: 'HannahUI',
       fileName: 'hannah-ui',
     },
     rollupOptions: {
-
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      external: [
+        'react',
+        'react-dom',
+        'react/jsx-runtime',
+        'react-router-dom',
+        'lucide-react',
+      ],
       output: {
-
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
           'react/jsx-runtime': 'react/jsx-runtime',
+          'react-router-dom': 'ReactRouterDOM',
+          'lucide-react': 'LucideReact',
         },
+        assetFileNames: (info) =>
+          info.names?.[0]?.endsWith('.css') ? 'hannah-ui.css' : (info.names?.[0] ?? 'asset'),
       },
     },
+    cssCodeSplit: false,
+    sourcemap: false,
   },
 })
