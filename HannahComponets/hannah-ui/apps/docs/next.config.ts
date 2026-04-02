aac45d4fc2aeae7c4a39110bf7224c9a2ad74f06
Owen-H123 <hugokuro0@gmail.com>
2026-04-02 14:00:03 -0500

    fix: resolviendo conflictos de rutas.

--- HannahComponets/hannah-ui/apps/docs/next.config.ts
+++ HannahComponets/hannah-ui/apps/docs/next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/sb-manager/:path*",
+        destination: "/storybook/sb-manager/:path*",
+      },
+      {
+        source: "/sb-addons/:path*",
+        destination: "/storybook/sb-addons/:path*",
+      },
+      {
+        source: "/sb-common-assets/:path*",
+        destination: "/storybook/sb-common-assets/:path*",
+      },
+      {
+        source: "/assets/:path*",
+        destination: "/storybook/assets/:path*",
+      },
+      {
+        source: "/iframe.html",
+        destination: "/storybook/iframe.html",
+      },
+      {
+        source: "/index.json",
+        destination: "/storybook/index.json",
+      },
+      {
+        source: "/project.json",
+        destination: "/storybook/project.json",
+      }
+    ];
+  },
 };
 
 export default nextConfig;
